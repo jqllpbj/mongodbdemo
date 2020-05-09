07a925a0228e1df6dcb2739ca7532e21e6e01519
jqllpbj <jqllpbj@126.com>
2020-05-09 09:56:19 +0800

    第1次提交

--- src/main/java/com/homolo/Task.java
+++ src/main/java/com/homolo/Task.java
@@ -1,19 +1,21 @@
 package com.homolo;
 
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Aggregates;
+
 
 
 import com.mongodb.client.model.Indexes;
-import com.mongodb.client.model.Sorts;
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
-import jdk.tools.jaotc.Main;
+
+
+
 import org.bson.Document;
 
-import java.sql.SQLOutput;
+
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -42,66 +44,88 @@ public class Task {
         Task task=new Task(database);
         task.show();
         client.close();
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("2");
-
 
     }
 
     public void show() throws ParseException {
+        Block<Document> printBlock = new Block<Document>() {
+            @Override
+            public void apply(Document document) {
+                System.out.println(document.toJson());
+            }
+        };
+
         /**
          * 1.实现新建数据库,新建集合,新建文档
          */
         //collection有则使用,没有则创建
         MongoCollection<Document> collection=database.getCollection("demo");
         Document document1=new Document("name","jack")
-                .append("age",12)
-                .append("emial","xxxx@example.com");
+                .append("job","engineer")
+                .append("birth",new SimpleDateFormat("yyyy-M-dd").parse("1995-1-23"))
+                .append("dept","1")
+                .append("salary",5000)
+                .append("remark","");
         collection.insertOne(document1);
+        collection.find().forEach(printBlock);
+        System.out.println("--------------------------------");
 
-        Document document2=new Document("name","jack")
-                .append("age",12)
-                .append("emial","xxxx@example.com");
+        Document document2=new Document("name","joh")
+                .append("job","engineer")
+                .append("birth",new SimpleDateFormat("yyyy-M-dd").parse("1995-1-24"))
+                .append("job","engineer")
+                .append("dept","1")
+                .append("salary",7000)
+                .append("remark","");
 
-        Document document3=new Document("name","jack")
-                .append("age",12)
-                .append("emial","xxxx@example.com");
+        Document document3=new Document("name","james")
+                .append("job","engineer")
+                .append("birth",new SimpleDateFormat("yyyy-M-dd").parse("1995-1-25"))
+                .append("job","engineer")
+                .append("dept","2")
+                .append("salary",6000)
+                .append("remark","");
 
-        Document document4=new Document("name","jack")
-                .append("age",12)
-                .append("emial","xxxx@example.com");
+        Document document4=new Document("name","jane")
+                .append("job","engineer")
+                .append("birth",new SimpleDateFormat("yyyy-M-dd").parse("1995-1-26"))
+                .append("job","engineer")
+                .append("dept","2")
+                .append("salary",1500)
+                .append("remark","");
         collection.insertMany(asList(document2,document3,document4));
+        collection.find().forEach(printBlock);
+
+        System.out.println("1--------------------------------");
 
 
-
-
-
-
-      /*  *//**
+        /**
          * 2.查询日期为2019年1月份的数据,并把查询出来的数据"备注"字段的值改为xxx修改
-         *//*
+         */
+
         SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
         Date startdate=dateFormat.parse("2019-1-1");
         Date enddate=dateFormat.parse("2019-1-31");
         collection.updateOne(
-                and(gte("createtime",startdate),lte("createtime",enddate)),
+                and(gte("birth",startdate),lte("birth",enddate)),
                 combine(set("remark","jqllpbj")));
+        collection.find().forEach(printBlock);
+        System.out.println("2--------------------------------");
 
-        *//**
-         * 3.distinct 实现一个功能
 
-         *//*
+
+        /**
+         *  3.distinct 实现一个功能
+         */
+
         collection.distinct("name",String.class);
+        collection.find().forEach(printBlock);
+        System.out.println("3--------------------------------");
 
 
-
-
-
-
-        *//**
+        /**
          * 4.aggregation
-         *//*
+         */
         collection.aggregate(
             asList(
                    match(
@@ -110,19 +134,21 @@ public class Task {
                                     gte("salary",5000),
                                     lte("salary",8000)
                             )),
-                    group("$deptment",
+                    group("$dept",
                             sum("totalSalary","$salary"),
                             avg("avageSalary","$salary"),
                             max("maxSalary","$salary")
                             ),
                     sort(ascending("name")),
-                    skip(2),
+                    skip(0),
                     limit(4)
             )
-        );
-        *//**
+        ).forEach(printBlock);
+        System.out.println("4--------------------------------");
+        /**
          * 5.index 实现索引的添加和删除
-         *//*
+         */
+
         collection.createIndex(Indexes.ascending("name","age"));
         collection.createIndex(
                Indexes.compoundIndex(
@@ -130,34 +156,43 @@ public class Task {
                        Indexes.descending("salary"))
                );
 
-        collection.dropIndex("name");
+        //collection.dropIndex("name");
         collection.dropIndexes();
         //
 
 
-        *//**
+        /**
+         * 6.query条件有"不等于",包含,不为空,或的练习
          *
-         * query条件有"不等于",包含,不为空,或的练习
-         *//*
-
+         */
         collection.find(
+                and(
+                        ne("name",""),
+                        ne("name",null)
+                )
+        ).forEach(printBlock);
+        System.out.println("5--------------------------------");
+        collection.find(in("name","jack")).forEach(printBlock);
+        System.out.println("6-------------------------------");
+
+
+       /* collection.find(
                 ne("name","john")
-        );
+        ).forEach(printBlock);
         collection.find(
                 ne("age",null)
-        );
+        ).forEach(printBlock);
         collection.find(
                 or(
                         in("name","jack","hijh"),
                         gte("salary",598))
-        );
+        ).forEach(printBlock);
         collection.find(
                 eq("name","Amarrcof")
         );
         collection.find(
                 in("name","jack","han","trsa")
-        );
-*/
+        );*/
 
     }
 
